82b6eaf4b9f80d8f3458dc6520bb2dc632533df6
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-11-21 00:39:20 +0800

    Add image test
    Fix test cmake file.
    Add image test.
    Add blank image for test.

---
+++ test/img_test.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include <laneft_ocv.hpp>
+
+using namespace std;
+using namespace cv;
+
+void draw_ctrl_simulation(Mat& img, int x, int y);
+
+int main(int argc, char* argv[])
+{
+	// Checking
+	if(argc <= 1)
+	{
+		cout << "Pass image to run the test" << endl;
+		return -1;
+	}
+
+	// Loop for testing
+	for(int i = 1; i < argc; i++)
+	{
+		// Load image
+		Mat src = imread(argv[i], CV_LOAD_IMAGE_COLOR);
+		if(src.empty())
+		{
+			cout << "Failed to load " << argv[i] << endl;
+			return -1;
+		}
+
+		// Process canny
+		Mat canny;
+		Canny(src, canny, 255, 255);
+
+		// Draw control simulation
+		laneft_ocv laneFt;
+        double ctrlFeature = laneFt.get_feature(canny);
+        laneFt.draw_line_onto(src);
+        draw_ctrl_simulation(src, src.cols / 2 - ctrlFeature, src.rows / 4 * 3);
+
+		// Show result
+		imshow("Result", src);
+		waitKey(0);
+	}
+
+	return 0;
+}
+
+void draw_ctrl_simulation(Mat& img, int x, int y)
+{
+    // Draw vertical line
+    line(img, Point(img.cols / 2, 0), Point(img.cols / 2, img.rows), Scalar(0, 0, 255), 2);
+
+    // Draw control point
+    circle(img, Point(x, y), 5, Scalar(0, 0, 255), 2);
+}
+
